77c73e312de19971ce7e60ec8354021f1a1984a4
TheMrChaptastic <chappy@themrchaptastic.com>
2022-02-14 11:32:20 -0600

    part 1 finished

---
+++ Testing/Models/ProductRepository.cs
@@ -0,0 +1,19 @@
+﻿using Dapper;
+using System.Collections.Generic;
+using System.Data;
+
+namespace Testing.Models
+{
+    public class ProductRepository : IProductRepository
+    {
+        public ProductRepository(IDbConnection conn)
+        {
+            _conn = conn;
+        }
+        private readonly IDbConnection _conn;
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return _conn.Query<Product>("SELECT * FROM products");
+        }
+    }
+}
